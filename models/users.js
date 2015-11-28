6f5627d5806e27c9c832dad51962fd3a1a06f0ac
Avinash <savinash47@gmail.com>
2015-11-28 14:42:18 -0800

    validae duplicate email

--- models/users.js
+++ models/users.js
@@ -15,5 +15,13 @@ module.exports = function(sequelize, DataTypes) {
 				len: [7,15]			
 			}
 		}
+	},{
+		hooks: {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string'){
+					user.email = user.email.toLowerCase();
+				}
+			}
+		}
 	});
 };
